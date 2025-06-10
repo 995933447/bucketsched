834f44a189488bdc5069fc93a916919c345efc27
995933447 <34621597+995933447@users.noreply.github.com>
2025-06-10 18:25:48 +0400

    Update queue.go

--- queue.go
+++ queue.go
@@ -1,23 +1,21 @@
-package bucketsched
+package bucketQueue
 
 import (
-	"github.com/995933447/log-go"
-	"github.com/995933447/log-go/impl/loggerwriter"
-	"github.com/995933447/std-go/print"
+	"errors"
 	uuid "github.com/satori/go.uuid"
 	"sync"
 	"time"
 )
 
-func NewBucketQueue(name string, concurWorkerNum uint32, logger *log.Logger) *BucketQueue {
+var ErrQueueStackFull = errors.New("queue stack full")
+
+// NewBucketQueue 普通队列, 总共concurWorkerNum个最大可用worker,根据bucket id公平权重调度
+func NewBucketQueue(name string, concurWorkerNum uint32) *BucketQueue {
 	if name == "" {
 		name = uuid.NewV4().String()
 	}
 	if concurWorkerNum == 0 {
 		concurWorkerNum = 1
-	}
-	if logger == nil {
-		logger = log.NewLogger(loggerwriter.NewStdoutLoggerWriter(print.ColorNil))
 	}
 	return &BucketQueue{
 		name:            name,
@@ -25,8 +23,16 @@ func NewBucketQueue(name string, concurWorkerNum uint32, logger *log.Logger) *Bu
 		taskCh:          make(chan *Task),
 		newTaskInSign:   make(chan struct{}),
 		concurWorkerNum: concurWorkerNum,
-		logger:          logger,
+		bucketConcurRec: map[int64]uint32{},
 	}
+}
+
+// NewBucketQueueWithBucketConcur bucket限制并发队列, 总共concurWorkerNum个最大可用worker,根据bucket id公平权重调度,
+// 且单个bucket, 同时只能有bucketConcurMax个消息被并发消费
+func NewBucketQueueWithBucketConcur(name string, concurWorkerNum, bucketConcurMax uint32) *BucketQueue {
+	queue := NewBucketQueue(name, concurWorkerNum)
+	queue.bucketConcurMax = bucketConcurMax
+	return queue
 }
 
 type Bucket struct {
@@ -45,11 +51,35 @@ type BucketQueue struct {
 	taskCh          chan *Task
 	newTaskInSign   chan struct{}
 	concurWorkerNum uint32
+	bucketConcurMax uint32
+	bucketConcurRec map[int64]uint32
 	mu              sync.Mutex
-	logger          *log.Logger
+	size            int
 }
 
-func (q *BucketQueue) Reg(task *Task) {
+func (q *BucketQueue) SetSize(size int) {
+	if size <= 0 {
+		return
+	}
+	q.size = size
+}
+
+func (q *BucketQueue) Size() int {
+	var queueSize int
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	for _, bucket := range q.bucketMap {
+		queueSize += len(*bucket.tasks)
+	}
+	return queueSize
+}
+
+func (q *BucketQueue) Reg(task *Task) error {
+	queueSize := q.Size()
+	if (q.size > 0 && q.size <= queueSize) || (q.size == 0 && queueSize > 2024000) {
+		return ErrQueueStackFull
+	}
+
 	if task.delayAt <= 0 {
 		if task.attempted == 0 {
 			task.delayAt = time.Now().Unix() + int64(task.delay)
@@ -71,7 +101,7 @@ func (q *BucketQueue) Reg(task *Task) {
 	bucket, ok := q.bucketMap[task.bucketId]
 	if ok {
 		bucket.tasks.pushTask(task)
-		return
+		return nil
 	}
 
 	bucket = &Bucket{
@@ -90,6 +120,11 @@ func (q *BucketQueue) Reg(task *Task) {
 	}
 
 	q.bucketMap[task.bucketId] = bucket
+	return nil
+}
+
+func (q *BucketQueue) hasBucketConcurLimit() bool {
+	return q.bucketConcurMax > 0
 }
 
 func (q *BucketQueue) Run() {
@@ -98,14 +133,16 @@ func (q *BucketQueue) Run() {
 			for {
 				task := <-q.taskCh
 				task.attempted++
-				q.logger.Infof(nil, "BucketQueue(name:%s) exec task.%s.id:%s, attempted:%d", q.name, task.name, task.taskId, task.attempted)
 				err := task.hdl(task)
+				if q.hasBucketConcurLimit() {
+					q.mu.Lock()
+					concurNum := q.bucketConcurRec[task.bucketId]
+					if concurNum > 0 {
+						q.bucketConcurRec[task.bucketId] = concurNum - 1
+					}
+					q.mu.Unlock()
+				}
 				if err != nil {
-					q.logger.Errorf(
-						nil,
-						"BucketQueue(name:%s) exec task.%s.id:%s, attempted:%d, FAILED!!reason:%v",
-						q.name, task.name, task.taskId, task.attempted, err,
-					)
 					if task.maxAttempt <= task.attempted {
 						continue
 					}
@@ -114,12 +151,24 @@ func (q *BucketQueue) Run() {
 					} else {
 						task.delayAt = time.Now().Unix() + int64(task.delay)
 					}
-					q.Reg(task)
+					if err = q.Reg(task); err != nil {
+						// TODO
+					}
 				}
 			}
 		}()
 	}
 	q.sched()
+}
+
+func (q *BucketQueue) waitNewTaskIn() {
+	waitNewTaskTimeoutTk := time.NewTimer(time.Second)
+	select {
+	case <-waitNewTaskTimeoutTk.C:
+		waitNewTaskTimeoutTk.Stop()
+	case <-q.newTaskInSign:
+		waitNewTaskTimeoutTk.Stop()
+	}
 }
 
 func (q *BucketQueue) sched() {
@@ -129,12 +178,33 @@ func (q *BucketQueue) sched() {
 		if q.curBucket == nil {
 			if q.bucketHeader == nil {
 				q.mu.Unlock()
-				time.Sleep(time.Second)
+				q.waitNewTaskIn()
 				continue
 			}
 			q.curBucket = q.bucketHeader
 		}
-		task, ok := q.curBucket.tasks.popTask()
+
+		var (
+			overBucketConcurMax bool
+			bucketConcurNum     uint32
+		)
+		limitedBucketConcur := q.hasBucketConcurLimit()
+		if limitedBucketConcur {
+			bucketConcurNum = q.bucketConcurRec[q.curBucket.bucketId]
+			overBucketConcurMax = bucketConcurNum >= q.bucketConcurMax
+		}
+
+		var (
+			ok   bool
+			task *Task
+		)
+		if !overBucketConcurMax {
+			task, ok = q.curBucket.tasks.popTask()
+			if ok && limitedBucketConcur {
+				q.bucketConcurRec[q.curBucket.bucketId] = bucketConcurNum + 1
+			}
+		}
+
 		q.mu.Unlock()
 		if ok {
 			loopEmptyCnt = 0
@@ -144,30 +214,41 @@ func (q *BucketQueue) sched() {
 			q.mu.Lock()
 			// 所有桶里都没有到期执行的任务，休眠1s
 			if loopEmptyCnt >= len(q.bucketMap) {
-				var rmBucketIds []int64
+				var rmBuckets []*Bucket
 				for _, bucket := range q.bucketMap {
 					if len(*bucket.tasks) == 0 {
-						rmBucketIds = append(rmBucketIds, bucket.bucketId)
+						rmBuckets = append(rmBuckets, bucket)
 					}
 				}
-				for _, bucketId := range rmBucketIds {
-					delete(q.bucketMap, bucketId)
+				for _, bucket := range rmBuckets {
+					if bucket.pre != nil {
+						bucket.pre.next = bucket.next
+					}
+					if bucket.next != nil {
+						bucket.next.pre = bucket.pre
+					}
+					if q.bucketHeader == bucket {
+						q.bucketHeader = bucket.next
+					}
+					if q.bucketTail == bucket {
+						q.bucketTail = bucket.pre
+					}
+					if q.curBucket == bucket {
+						q.curBucket = bucket.next
+					}
+					delete(q.bucketMap, bucket.bucketId)
 				}
 				q.mu.Unlock()
-				waitNewTaskTimeoutTk := time.NewTimer(time.Second)
-				select {
-				case <-waitNewTaskTimeoutTk.C:
-					waitNewTaskTimeoutTk.Stop()
-				case <-q.newTaskInSign:
-					waitNewTaskTimeoutTk.Stop()
-				}
+				q.waitNewTaskIn()
 				loopEmptyCnt = 0
 			} else {
 				q.mu.Unlock()
 			}
 		}
 		q.mu.Lock()
-		q.curBucket = q.curBucket.next
+		if q.curBucket != nil {
+			q.curBucket = q.curBucket.next
+		}
 		q.mu.Unlock()
 	}
 }
